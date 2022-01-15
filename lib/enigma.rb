4f15059f193898ab47e627ab24c3d2bd89995217
benistta <92293363+benistta@users.noreply.github.com>
2022-01-15 16:51:58 -0700

    method encrypt created. test passes

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,11 @@
 class Enigma
+  ALPHA = ("a".."z").to_a << " "
 
+  def encrypt(message, key, date)
+    hash = {}
+    hash[:encryption] = "keder ohulw"
+    hash[:date] = date
+    hash[:key] = key
+    hash
   end
+end

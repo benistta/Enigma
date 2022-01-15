978a698251d3e163ee293b03712e100be4b72afb
benistta <92293363+benistta@users.noreply.github.com>
2022-01-15 16:53:50 -0700

    method key to keys passes test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,4 +8,15 @@ class Enigma
     hash[:key] = key
     hash
   end
+
+  #lets find the keys and the offsets
+  def key_to_keys(key)
+    keys_array = []
+    # key[0,1]
+    keys_array << key[0..1].to_i
+    keys_array << key[1..2].to_i
+    keys_array << key[2..3].to_i
+    keys_array << key[3..4].to_i
+    keys_array
+  end
 end

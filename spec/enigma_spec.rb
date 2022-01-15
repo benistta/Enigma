8e152262d2874d4ec7c5b25c1e047c506c8dd23b
benistta <92293363+benistta@users.noreply.github.com>
2022-01-15 16:54:50 -0700

    test for offsets created

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,16 +9,21 @@ RSpec.describe Enigma do
   end
 
   it 'encrypted message' do
-  enigma = Enigma.new
-  expect(enigma.encrypt("hello world", "02715", "040895")).to eq({
-      encryption: "keder ohulw",
-      key: "02715",
-      date: "040895"
-    })
+    enigma = Enigma.new
+    expect(enigma.encrypt("hello world", "02715", "040895")).to eq({
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      })
   end
 
   it 'it can get keys from key' do
-  enigma = Enigma.new
-  expect(enigma.key_to_keys("02715")).to eq([2, 27, 71, 15])
+    enigma = Enigma.new
+    expect(enigma.key_to_keys("02715")).to eq([2, 27, 71, 15])
+  end
+
+  it 'it can get offsets from date' do
+    enigma = Enigma.new
+    expect(enigma.date_to_offset("040895")).to eq([1, 0, 2, 5])
   end
 end

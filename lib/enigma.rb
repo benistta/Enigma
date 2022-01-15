9b57556ffa31f6ded1677bcd8cca03dfe3df4d82
benistta <92293363+benistta@users.noreply.github.com>
2022-01-15 16:55:46 -0700

    method date to offset passes test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -19,4 +19,13 @@ class Enigma
     keys_array << key[3..4].to_i
     keys_array
   end
+
+  def date_to_offset(date)
+    date_integer = date.to_i * date.to_i
+    last_four = date_integer.to_s[-4..-1]
+    seperated_last_four = last_four.chars
+    seperated_last_four.map do |element|
+      element.to_i
+    end
+  end
 end

5eb3928a3a604a50d85e50738fc2bbaf3c5728e0
benistta <92293363+benistta@users.noreply.github.com>
2022-01-15 16:51:09 -0700

    new test encrypted message created

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,4 +1,4 @@
-require 'date' # use Date.today 
+require 'date' # use Date.today
 require './lib/enigma'
 
 
@@ -6,5 +6,14 @@ RSpec.describe Enigma do
   it 'exists' do
     enigma = Enigma.new
     expect(enigma).to be_instance_of(Enigma)
+  end
+
+  it 'encrypted message' do
+  enigma = Enigma.new
+  expect(enigma.encrypt("hello world", "02715", "040895")).to eq({
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    })
   end
 end

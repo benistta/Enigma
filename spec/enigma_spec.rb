2d488c2cd474d8ce1f6b2bbacfabf3ae30b2873d
benistta <92293363+benistta@users.noreply.github.com>
2022-01-16 17:14:38 -0700

    Feat: Created test for the use of symbols

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -109,4 +109,13 @@ RSpec.describe Enigma do
       })
   end
 
+  it 'it can keep punctuation in place' do
+    enigma = Enigma.new
+    expected = {
+        encryption: "keder ohulw!",
+        key: "02715",
+        date: "040895"
+      }
+      expect(enigma.encrypt("hello world!", "02715", "040895")).to eq(expected)
+   end
 end

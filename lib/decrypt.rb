d0812a534b6abdc59453853416e49113f977282b
benistta <92293363+benistta@users.noreply.github.com>
2022-01-17 09:12:44 -0700

    Feat: CLI for encryption and decryption working

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,1 +1,13 @@
+require './lib/enigma'
 
+enigma = Enigma.new
+key = ARGV[2]
+date = ARGV[3]
+
+decryption = enigma.decrypt(File.read(ARGV[0]), key, date)
+
+File.open(ARGV[1], "w") do |file|
+
+  file.puts decryption[:decryption]
+  puts "Created #{ARGV[1]} with the key #{decryption[:key]} and date #{decryption[:date]}"
+end
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,1 +1,14 @@
+require './lib/enigma'
 
+enigma = Enigma.new
+
+handle = File.open(ARGV[0], "r")
+message = handle.read.strip
+handle.close
+
+encrypted_message = enigma.encrypt(message)
+puts "Created #{ARGV[1]} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
+
+writer = File.open(ARGV[1], "w")
+writer.write(encrypted_message[:encryption])
+writer.close
